3c286dfad0840ce7e6eecc7d443378c3346910a1
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-02-13 12:31:24 +0400

    fix directories

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,5 +1,4 @@
  import View from './View.js';
-import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class PreviewView extends View {
     _parentElement = '';
@@ -22,7 +21,6 @@ class PreviewView extends View {
             this._data.key ? '' : 'hidden'
         }">
               <svg>
-              <use href="${icons}#icon-user"></use>
               </svg>
             </div>
           </div>
